0e96b6a444a1a2afe7428306b8a13241bbfecb52
Dylan Michael Rodriquez <drodriqu@xinu20.cs.purdue.edu>
2018-01-29 00:15:03 -0500

    icmpv6 struct

--- cs636-base/include/icmpv6.h
+++ cs636-base/include/icmpv6.h
@@ -1,41 +1,50 @@
 /* icmpv6.h - definintions for the Internet Control Message Protocol */
 
+/*
+ * must be preceded by an ipv6 extension header
+ *
+ * The structure for an ICMP message:
+ *
+ * The IPv6 header contains source and destination
+ * information as well as information for an extension
+ * header. This extension header contains the code, 58.
+ * The IP header functions as the pseudo header for the
+ * ICMPv6 header. The header for ICMPv6 contains the type,
+ * code, and checksum. The payload then follows.
+ *
+ * Source: RFC 4443. Pg 3.
+ * RFC 4861 pg 18-18.
+ *
+ * */
 
-enum errormsg{
-    UNREACHABLE,
-    TOO_BIG,
-    TIME_EXCEEDED,
-    PARAM_PROB,
-    PRIVATE_EXP1,
-    PRIVATE_EXP2
+
+#define MAX_PAYLOAD (64)*(1000)
+#define DESTUNREACH 1
+#define PKT_BIG     2
+#define TIME_EX     3
+#define PVT1        100
+#define PVT2        100
+
+
+/*solicitation*/
+
+#define ROUTERS     133
+#define ROUTERA     134
+#define NEIGHBS     135
+#define NEIGHBA     136
+
+/*informational messages */
+#define ECHOREQ     128
+#define ECHOREP     129
+#define PVT3        200
+#define PVT4        201
+
+extern struct icmpv6{
+                         /*header*/
+    uint32 type;
+    uint32 code;
+    uint32 checksum;
+                         /*begin payload*/
+    byte payload[MAX_PAYLOAD];
 };
-
-enum info{
-    ECHOREQ,
-    ECHOREP,
-    PRIVATE_EXP1,
-    PRIVATE_EXP2
-}
-
-enum itype{
-    NSOLICIT,
-    NADVERTISE,
-    RSOLICIT,
-    RADVERTISE
-}
-
-typedef struct {
-    unsigned int icmpv6src:    128; /*source addr*/
-    unsigned int icmpv6dst:    128; /*dest addr*/
-    unsigned int icmpv6len:    32;  /*len of icmp msg */
-    unsigned int zero:         24;  /*init w zeros */
-    unsigned int next:         8;   /* nxt header */
-} icmpv6hdr;
-
-typedef struct{
-   unsigned int itype:      8; /*type code */
-   unsigned int code:       8; /* icmp code */
-   unsigned int cksum:      16; /* checksum */
-   unsigned int message:    32; /* icmp msg */
-} icmpv6msg;
 
